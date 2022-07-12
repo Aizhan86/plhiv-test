f511ba5164149df8bce95b4a12e7a9be39a9de4d
Aizhan <orinbayeva@mail.ru>
2022-07-12 11:43:14 +0600

    changed settings for CI

--- conftest.py
+++ conftest.py
@@ -26,18 +26,18 @@ def browser():
     options = Options()
     options.add_argument("--start-maximized")
     options.add_argument('--no-sandbox')
-    # options.add_argument('--headless')
+    options.add_argument('--headless')
     options.add_argument("--disable-extensions")
-    # options.add_argument('--disable-gpu')
+    options.add_argument('--disable-gpu')
     options.add_argument('--disable-dev-shm-usage')
     # options.add_argument('--proxy-server=http://%s' % proxy)
     options.add_argument("test-type")
     # capabilities.setCapability("chrome.binary", "<Path to binary>")
-    # capabilities = options.to_capabilities()
+    capabilities = options.to_capabilities()
 
     # browser = webdriver.Remote(desired_capabilities=DesiredCapabilities.CHROME, command_executor="http://127.0.0.1:4444/wd/hub")
-    # browser = webdriver.Remote(desired_capabilities=capabilities, command_executor="http://172.18.0.1:4444/wd/hub")
-    browser = webdriver.Chrome('/home/user/tool/chromedriver', chrome_options=options)
+    browser = webdriver.Remote(desired_capabilities=capabilities, command_executor="http://172.18.0.1:4444/wd/hub")
+    # browser = webdriver.Chrome('/home/user/tool/chromedriver', chrome_options=options)
 
     # pytest --dist=loadscope --tx 5*popen//python=python3.10 -n 5 --reruns 2 --only-rerun JavascriptException --only-rerun ElementClickInterceptedException testing_test.py
 
